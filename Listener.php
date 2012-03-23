dfbb9c6fa977cc892312a79927a0eb2c20bbab4b
Dao Hoang Son <daohoangson@gmail.com>
2012-03-23 22:45:15 +0700

    Improved inject function (template hook and stuff)

--- Listener.php
+++ Listener.php
@@ -36,28 +36,61 @@ class DevHelper_Listener {
 	public static function template_hook($hookName, &$contents, array $hookParams, XenForo_Template_Abstract $template) {
 		switch ($hookName) {
 			case 'devhelper_search_and_replace':
-				$markup = self::_getSearchAndReplaceMarkup();
+				$markup = '<!-- search and replace -->';
 				$contents = $markup . $contents . $markup;
 				break;
 		}
 	}
 	
-	protected static function _injectHtml(&$content, $html) {
-		if (empty($html)) return;
+	protected static function _injectHtml(&$target, $html, $offsetInTarget = 0,
+		$mark = '<!-- search and replace -->', $revertMark = '<!-- revert all the thing! -->'
+	) {
+		if ($offsetInTarget === false) return; // do nothing if invalid offset is given
+		if (empty($html)) return; // the html is empty
 		
-		$markup = self::_getSearchAndReplaceMarkup();
-		$pos1 = strpos($html, $markup);
-		if ($pos1 !== false) {
-			$pos2 = strpos($html, $markup, $pos1 + 1);
-			if ($pos2 !== false) {
-				// found the 2 markup positions
-				$search = substr($html, $pos1 + strlen($markup), $pos2 - $pos1 - strlen($markup));
-				$content = str_replace($search, $html, $content);
+		$injected = false;
+		$isRevert = (strpos($html, $revertMark) !== false);
+		
+		$startPos = strpos($html, $mark);	
+		if ($startPos !== false) {
+			$endPos = strpos($html, $mark, $startPos + 1);
+			if ($endPos !== false) {
+				// found the two marks
+				$markLen = strlen($mark);
+				$marked = trim(substr($html, $startPos + $markLen, $endPos - $startPos - $markLen));
+				
+				if (!$isRevert) {
+					// normal mode, look for the first occurence
+					$markedPos = strpos($target, $marked, $offsetInTarget);
+				} else {
+					// revert mode, look for the last occurence
+					$markedPos = strrpos($target, $marked, $offsetInTarget);
+				}
+
+				if ($markedPos !== false) {
+					// the marked text has been found
+					// start injecting our html in place
+					$html = str_replace($mark, '', $html);
+					$html = str_replace($revertMark, '', $html);
+					
+					$target = substr_replace($target, $html, $markedPos, strlen($marked));
+				}
+				
+				$injected = true; // assume that it was injected
 			}
 		}
-	}
-	
-	protected static function _getSearchAndReplaceMarkup() {
-		return '<!-- search and replace -->';
+		
+		if (!$injected) {
+			$html = str_replace($mark, '', $html);
+			$html = str_replace($revertMark, '', $html);
+			
+			if (!$isRevert) {
+				//  normal mode, append the html
+				$target .= $html;
+			} else {
+				// revert mode, insert instead of append
+				$target = $html . $target;
+			}
+		}
 	}
 }
