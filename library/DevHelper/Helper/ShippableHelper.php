b0c86eccf08399f6865d60e9af134a755cbb0ca7
Dao Hoang Son <daohoangson@gmail.com>
2015-10-16 14:08:58 +0700

    improved shippable helper updater

--- library/DevHelper/Helper/ShippableHelper.php
+++ library/DevHelper/Helper/ShippableHelper.php
@@ -26,10 +26,33 @@ class DevHelper_Helper_ShippableHelper
             $targetContents = utf8_substr_replace($targetContents, $replacement, $pos, utf8_strlen($php));
         }
 
+        $classPrefix = substr($targetClass, 0, strpos($targetClass, 'ShippableHelper_'));
+        $offset = 0;
+        while (true) {
+            if (!preg_match('#DevHelper_Helper_ShippableHelper_[a-zA-Z_]+#', $targetContents,
+                $matches, PREG_OFFSET_CAPTURE, $offset)
+            ) {
+                break;
+            }
+
+            $siblingSourceClass = $matches[0][0];
+            $offset = $matches[0][1];
+            $siblingTargetClass = str_replace('DevHelper_Helper_', $classPrefix, $siblingSourceClass);
+            $targetContents = substr_replace($targetContents, $siblingTargetClass, $offset, strlen($siblingSourceClass));
+
+            class_exists($siblingTargetClass);
+
+            $offset += 1;
+        }
+
+        $targetContents = preg_replace('#\* @version \d+\s*\n#', '$0 * @see ' . $sourceClass . "\n",
+            $targetContents, -1, $count);
+
         return DevHelper_Generator_File::filePutContents($targetPath, $targetContents);
     }
 
-    public static function checkForUpdate($path) {
+    public static function checkForUpdate($path)
+    {
         if (strpos($path, '/ShippableHelper/') !== false) {
             $contents = DevHelper_Generator_File::fileGetContents($path);
 
