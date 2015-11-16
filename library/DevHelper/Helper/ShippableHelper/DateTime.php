5fc74f8a6f5ca9718eaa1c272edbb359f9638be7
Dao Hoang Son <daohoangson@gmail.com>
2015-11-17 00:45:01 +0700

    [ShippableHelper_DateTime] v2

--- library/DevHelper/Helper/ShippableHelper/DateTime.php
+++ library/DevHelper/Helper/ShippableHelper/DateTime.php
@@ -2,7 +2,7 @@
 
 /**
  * Class DevHelper_Helper_ShippableHelper_DateTime
- * @version 1
+ * @version 2
  */
 class DevHelper_Helper_ShippableHelper_DateTime
 {
@@ -33,5 +33,42 @@ class DevHelper_Helper_ShippableHelper_DateTime
         }
 
         return $zone;
+    }
+
+    /**
+     * Get Unix timestamp in GMT for inputted date values.
+     *
+     * @param int|array $year in user timezone
+     * @param int $month in user timezone
+     * @param int $day in user timezone
+     * @param int $hour in user timezone
+     * @param int $minute in user timezone
+     * @param int $second in user timezone
+     *
+     * @return int timestamp in GMT
+     */
+    public static function gmmktime($year, $month = 0, $day = 0, $hour = 0, $minute = 0, $second = 0)
+    {
+        if (is_array($year)) {
+            $args = func_get_args();
+            if (count($args) !== 1) {
+                return 0;
+            }
+
+            $values = $year;
+            $year = 0;
+            foreach (array('year', 'month', 'day', 'hour', 'minute', 'second') as $key) {
+                if (isset($values[$key])
+                    && is_int($values[$key])
+                ) {
+                    $$key = intval($values[$key]);
+                }
+            }
+        }
+
+        $timestamp = gmmktime($hour, $minute, $second, $month, $day, $year);
+        $offset = XenForo_Locale::getTimeZoneOffset();
+
+        return $timestamp - $offset;
     }
 }
