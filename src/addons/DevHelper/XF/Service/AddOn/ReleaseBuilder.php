391000596e1e418beb3cfc45a06ca83d50bff721
Dao Hoang Son <daohoangson@gmail.com>
2017-09-07 00:04:48 +0700

    Simplify add-on release builder

--- src/addons/DevHelper/XF/Service/AddOn/ReleaseBuilder.php
+++ src/addons/DevHelper/XF/Service/AddOn/ReleaseBuilder.php
@@ -7,57 +7,6 @@ use XF\Util\File;
 
 class ReleaseBuilder extends XFCP_ReleaseBuilder
 {
-    protected function prepareFilesToCopy()
-    {
-        parent::prepareFilesToCopy();
-
-        $srcUptoNamespace = $this->addOnRoot;
-        $srcUptoNamespaceLocated = StreamWrapper::locate($srcUptoNamespace);
-        $repoPath = preg_replace('#/src/addons.+$#', '', $srcUptoNamespaceLocated);
-        $ds = DIRECTORY_SEPARATOR;
-
-        $dirPaths = [];
-        foreach (new \DirectoryIterator($repoPath) as $dirCandidate) {
-            if (!$dirCandidate->isDir()
-                || $dirCandidate->isDot()) {
-                continue;
-            }
-
-            $skip = false;
-            $basename = $dirCandidate->getBasename();
-            switch ($basename) {
-                case '.git':
-                case 'addons':
-                case 'dev-scripts':
-                case 'library':
-                case 'src':
-                case 'xenforo':
-                    $skip = true;
-                    break;
-            }
-            if ($skip) {
-                continue;
-            }
-
-            $dirPaths[] = $dirCandidate->getPathname();
-        }
-
-        foreach ($dirPaths as $dirPath) {
-            $dirStandardized = $this->standardizePath($repoPath, $dirPath);
-
-            $filesIterator = $this->getFileIterator($dirPath);
-            foreach ($filesIterator AS $file) {
-                if ($file->isDir()) {
-                    continue;
-                }
-
-                $fileStandardized = $this->standardizePath($dirPath, $file->getPathname());
-                $destination = $this->uploadRoot . $ds . $dirStandardized . $ds . $fileStandardized;
-                File::copyFile($file->getPathname(), $destination, false);
-            }
-        }
-    }
-
     public function finalizeRelease()
     {
         $oopsPath = $this->buildRoot . '/oops';
@@ -65,6 +14,15 @@ class ReleaseBuilder extends XFCP_ReleaseBuilder
         $this->buildRoot = $oopsPath;
 
         parent::finalizeRelease();
+    }
+
+    protected function getExcludedDirectories()
+    {
+        $dirs = parent::getExcludedDirectories();
+
+        $dirs[] = '.git';
+
+        return $dirs;
     }
 }
 
