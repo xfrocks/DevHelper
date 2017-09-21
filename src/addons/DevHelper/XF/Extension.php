e8421b3d71d30cb71a6724cfc1e90a86c96ae332
Dao Hoang Son <daohoangson@gmail.com>
2017-09-21 19:01:17 +0700

    Fix http client issue because of our stream wrapper

--- src/addons/DevHelper/XF/Extension.php
+++ src/addons/DevHelper/XF/Extension.php
@@ -6,12 +6,24 @@ use DevHelper\StreamWrapper;
 
 class Extension extends \XF\_Extension
 {
+    /**
+     * Extension constructor.
+     * @param array $listeners
+     * @param array $classExtensions
+     *
+     * @see \DevHelper\Listener::httpClientConfig()
+     * @see \DevHelper\XF\AddOn\AddOn
+     * @see \DevHelper\XF\AddOn\Manager
+     * @see \DevHelper\XF\Service\AddOn\ReleaseBuilder
+     */
     public function __construct(array $listeners = [], array $classExtensions = [])
     {
         parent::__construct($listeners, $classExtensions);
 
         stream_wrapper_register(StreamWrapper::PROTOCOL, StreamWrapper::class);
         XF::setSourceDirectory(StreamWrapper::PROTOCOL . '://src');
+
+        $this->addListener('http_client_config', ['DevHelper\\Listener', 'httpClientConfig']);
 
         $classes = [
             'XF\AddOn\AddOn',
