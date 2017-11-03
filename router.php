4d4a838e04071f4812ef9f9a8aeeab21a0e18a7d
Dao Hoang Son <daohoangson@gmail.com>
2017-11-03 14:33:27 +0700

    Docker version bump (php v7.1.11)

--- router.php
+++ router.php
@@ -1,4 +1,10 @@
 <?php
 
+if (empty($_SERVER['DEVHELPER_PHP_APACHE_VERSION_ID']) ||
+    $_SERVER['DEVHELPER_PHP_APACHE_VERSION_ID'] !== '2017110301'
+) {
+    die('Please make sure the Docker container is up to date.');
+}
+
 require('./src/addons/DevHelper/Router.php');
 \DevHelper\Router::route(__FILE__);
