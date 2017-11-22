1399a7739c7aea3fe3a090642a3bf6e16bcc6c8f
Dao Hoang Son <daohoangson@gmail.com>
2017-11-22 09:55:32 +0700

    Add support for cmd.php xf-addon:xxx shortcuts

--- router.php
+++ router.php
@@ -1,10 +1,10 @@
 <?php
 
 if (empty($_SERVER['DEVHELPER_PHP_APACHE_VERSION_ID']) ||
-    $_SERVER['DEVHELPER_PHP_APACHE_VERSION_ID'] !== '2017110301'
+    $_SERVER['DEVHELPER_PHP_APACHE_VERSION_ID'] !== '2017112201'
 ) {
     die('Please make sure the Docker container is up to date.');
 }
 
-require('./src/addons/DevHelper/Router.php');
+require(__DIR__ . '/src/addons/DevHelper/Router.php');
 \DevHelper\Router::route(__FILE__);
