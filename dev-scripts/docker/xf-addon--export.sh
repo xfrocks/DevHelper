cb00474581c1b3d4ef833c2811d4da39dff5106d
Dao Hoang Son <daohoangson@gmail.com>
2017-12-17 13:27:46 +0700

    Integrate phpcbf into xf-addon--export.sh

--- dev-scripts/docker/xf-addon--export.sh
+++ dev-scripts/docker/xf-addon--export.sh
@@ -8,7 +8,20 @@ if [ -z "${_addOnId}" ]; then
   exit 1
 fi
 
-xf-addon--phpcs.sh "${_addOnId}"
+_phpcs=$( xf-addon--phpcs.sh "${_addOnId}" || true )
+if [ ! -z "$_phpcs" ]; then
+  echo "$_phpcs"
+
+  _phpcbfSuggestion=$( echo "$_phpcs" | grep 'PHPCBF CAN FIX' )
+  if [ ! -z "$_phpcbfSuggestion" ]; then
+    xf-addon--phpcbf.sh "${_addOnId}"
+    echo 'phpcs failed, phpcbf OK'
+    exit 2
+  fi
+
+  echo 'phpcs failed'
+  exit 1
+fi
 echo 'phpcs OK'
 
 exec cmd-php.sh xf-addon:export "${_addOnId}"
