68bccc80e63b0a9c0d0809f315e666a56445c5fa
Dao Hoang Son <daohoangson@gmail.com>
2018-03-30 09:42:17 +0700

    Add support for uid 1000

--- router.php
+++ router.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_SERVER['DEVHELPER_PHP_APACHE_VERSION_ID'])) {
-    $versionExpected = '2018032901';
+    $versionExpected = '2018033001';
     $versionActual = $_SERVER['DEVHELPER_PHP_APACHE_VERSION_ID'];
     if ($versionActual !== $versionExpected) {
         die(sprintf('Please rebuild Docker image. Expected %s, actual %s', $versionExpected, $versionActual));
