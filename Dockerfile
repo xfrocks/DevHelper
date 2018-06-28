4d39ed18efa414437f03f911b56dbe0807e95e1a
Dao Hoang Son <daohoangson@gmail.com>
2018-06-28 13:38:22 +0700

    Update php-apache-7.2.7, now with composer.
    Related to vimobi/xenforo2#2

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM xfrocks/xenforo:php-apache-7.2.4
+FROM xfrocks/xenforo:php-apache-7.2.7
 
-ENV DEVHELPER_PHP_APACHE_VERSION_ID 2018041701
+ENV COMPOSER_ALLOW_SUPERUSER 1
+ENV DEVHELPER_PHP_APACHE_VERSION_ID 2018062801
 
 COPY docker/* /usr/local/bin/
 RUN /usr/local/bin/build.sh
--- prepend.php
+++ prepend.php
@@ -4,7 +4,7 @@
 
 function DevHelper_verifyPhpApacheVersionId()
 {
-    $versionExpected = '2018041701';
+    $versionExpected = '2018062801';
     $versionActual = $_ENV['DEVHELPER_PHP_APACHE_VERSION_ID'];
     if ($versionActual !== $versionExpected) {
         die(sprintf('Please rebuild Docker image. Expected version %s, actual %s', $versionExpected, $versionActual));
