6e83a4b7e594668658bfb1b0339c1fa0d1aef4df
Dao Hoang Son <daohoangson@gmail.com>
2018-08-18 16:24:20 +0700

    Deprecate xf-addon--export.sh

--- docker/xf-addon--export.sh
+++ docker/xf-addon--export.sh
@@ -8,25 +8,7 @@ if [ -z "${_addOnId}" ]; then
   exit 1
 fi
 
-devhelper-autogen.sh "${_addOnId}"
-devhelper-autocheck.sh "${_addOnId}"
-
-_addOnDir="/var/www/html/src/addons/${_addOnId}"
-_phpcs=$( devhelper-phpcs.sh "${_addOnDir}" 2>&1 || true )
-if [ ! -z "$_phpcs" ]; then
-  echo "$_phpcs"
-
-  _phpcbfSuggestion=$( echo "$_phpcs" | grep 'PHPCBF CAN FIX' )
-  if [ ! -z "$_phpcbfSuggestion" ]; then
-    echo "phpcs failed, execute \`devhelper-phpcbf.sh ${_addOnDir}\` to attempt fixing automatically" >&2
-    exit 2
-  fi
-
-  echo 'phpcs failed' >&2
-  exit 1
-fi
-echo 'phpcs OK'
-
-devhelper-phpstan.sh "${_addOnDir}"
-
-exec cmd-php.sh xf-addon:export "${_addOnId}"
+echo 'xf-addon--export.sh is no longer functional,'
+echo "Use \`cmd-php--xf-addon.sh export ${_addOnId}\` if you have to..."
+echo "For other usages, please use \`xf-dev--export--addon.sh ${_addOnId}\` instead."
+exit 1
--- docker/xf-dev--export--addon.sh
+++ docker/xf-dev--export--addon.sh
@@ -8,4 +8,25 @@ if [ -z "${_addOnId}" ]; then
   exit 1
 fi
 
+devhelper-autogen.sh "${_addOnId}"
+devhelper-autocheck.sh "${_addOnId}"
+
+_addOnDir="/var/www/html/src/addons/${_addOnId}"
+_phpcs=$( devhelper-phpcs.sh "${_addOnDir}" 2>&1 || true )
+if [ ! -z "$_phpcs" ]; then
+  echo "$_phpcs"
+
+  _phpcbfSuggestion=$( echo "$_phpcs" | grep 'PHPCBF CAN FIX' )
+  if [ ! -z "$_phpcbfSuggestion" ]; then
+    echo "phpcs failed, execute \`devhelper-phpcbf.sh ${_addOnDir}\` to attempt fixing automatically" >&2
+    exit 2
+  fi
+
+  echo 'phpcs failed' >&2
+  exit 1
+fi
+echo 'phpcs OK'
+
+devhelper-phpstan.sh "${_addOnDir}"
+
 exec cmd-php.sh xf-dev:export --addon "${_addOnId}"
