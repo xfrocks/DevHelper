4c7de305a532ad933eee7f69130ce246faf524a3
Dao Hoang Son <daohoangson@gmail.com>
2018-12-06 15:24:57 +0700

    Update php base image (for sockets extension)

--- router.php
+++ router.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_SERVER['DEVHELPER_PHP_APACHE_VERSION_ID'])) {
-    $versionExpected = '2018062801';
+    $versionExpected = '2018120601';
     $versionActual = $_SERVER['DEVHELPER_PHP_APACHE_VERSION_ID'];
     if ($versionActual !== $versionExpected) {
         die(sprintf('Please rebuild Docker image. Expected %s, actual %s', $versionExpected, $versionActual));
